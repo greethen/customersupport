a32a64aaa7bd529336ee3710c1196c05f47613ce
greethen <gret2539@stud.kea.dk>
2018-05-25 14:15:57 +0200

    sorted list with colours and importance

--- main.js
+++ main.js
@@ -10,12 +10,29 @@ function getAllCases(){
     }
     getAllCases();
 
+    //GRAB ELEMENTS from JSON
+    //CLONE
+    //PASTE (APPEND) IN TEMPLATE, so it shows
+
     function show(requests){
         console.log(requests)
         let caseTemplate = document.querySelector('.case_template').content;
+
+        requests.sort(function(a,b) {
+            //console.log("a:",a);
+            //console.log("b", b);
+
+            if( a.importance < b.importance ) {
+                return 1;
+            } else {
+                return -1;
+            }
+        })
+
         requests.forEach(request=>{
             console.log(request)
             let clone = caseTemplate.cloneNode(true);
+            //show the middle name if it exists
             if(request.middle){
                 //console.log("has middle", request.middle)
                 clone.querySelector('.name').textContent=`${request.first} ${request.middle} ${request.last}`;
@@ -23,13 +40,35 @@ function getAllCases(){
                 //console.log("no middle")
                 clone.querySelector('.name').textContent=`${request.first} ${request.last}`;
             }
-            
+            //show the date
+            clone.querySelector(".date").textContent=`${request.time.day} - ${request.time.month} - ${request.time.year}`;
+            //show the message
+            clone.querySelector(".message").textContent=request.message;
+            //show importance
+            clone.querySelector(".importance").textContent=request.importance;
+            if(request.importance<40){
+                    //then show green
+                    clone.querySelector(".importance").style.color = "#32FF4F";
+            }
+            else if(request.importance>70){
+                    //red
+                    clone.querySelector(".importance").style.color = "#E82C0C";
+            }
+            else {
+                    //yellow
+                    clone.querySelector(".importance").style.color = "#FDFF4B";
+            }
+
+            // more-button
+            clone.querySelector(".more").addEventListener("click", function(e) {
+                console.log("Clicked more-button");
+                console.log(e);
+            })
+
             document.querySelector("#cases").appendChild(clone)
         });
     }
-    //GRAB ELEMENTS from JSON
-    //CLONE
-    //PASTE IN TEMPLATE, so it shows
+
 
 
     // let caseTemplate = document.querySelector('.case_template').content;
